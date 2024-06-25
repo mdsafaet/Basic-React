6f9199397b6ebe44a3bc2a979443ebe444ac22bd
mdsafaet <mdsafaet84@gmail.com>
2024-06-26 00:24:23 +0600

    Create & declare components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+ 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>i am a react person</p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
+}
+function Person() {
+  const personStyle ={
+    border: '2px solid yellow',
+    margin: '10px'
+  }
+  return (
+  <div style={personStyle}>
+  <h1>Name : Safaet</h1>
+  <h1>Job: Student</h1>
+  </div>
+  )
+
 }
 
 export default App;
