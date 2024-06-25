a6f938430961159529fa4013c42a42d8ef2e8425
mdsafaet <mdsafaet84@gmail.com>
2024-06-26 01:16:53 +0600

    pass object to components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@
 import './App.css';
 
 function App() {
+  const products = [
+    {name: 'Phone', price: '$90.99'},
+    {name: 'Candy', price: '$60.99'},
+    {name: 'Chair', price: '$6.99'},
+    {name: ' Books', price: '$249.99'}
+  ]
  
   return (
     <div className="App">
       <header className="App-header">
         <p>i am a react person</p>
+        <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product>
+        <Product product={products[2]}></Product>
+        <Product product={products[3]}></Product>
         <Person name='Safaet' job='Student'></Person>
         <Person name='Md' job='Student'></Person>
         <Person name='Kashem' job='Student'></Person>
@@ -16,6 +26,33 @@ function App() {
     </div>
   );
 }
+function Product(props) {
+const productStyle = {
+  border: '2px solid gray',
+  borderRadius: '5px',
+  backgroundColor: 'blue',
+  height: '200px',
+  width: '200px',
+  float: 'left'
+}
+
+const {name, price} = props.product;
+console.log(name, price);
+
+return(
+  <div style={productStyle}>
+    <h3>{name}</h3>
+    <h5>{price}</h5>
+    <button>Buy Now</button>
+  </div>
+)
+}
+
+
+
+
+
+
 function Person(props
 ) {
   const personStyle ={
