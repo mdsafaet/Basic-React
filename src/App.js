39c0c993dae5a5f23a8c0e502859f1afd8014174
mdsafaet <mdsafaet84@gmail.com>
2024-06-26 00:50:49 +0600

    pass dynamic data to components

--- src/App.js
+++ src/App.js
@@ -7,22 +7,25 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>i am a react person</p>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
+        <Person name='Safaet' job='Student'></Person>
+        <Person name='Md' job='Student'></Person>
+        <Person name='Kashem' job='Student'></Person>
+        <Person name='Md.Safaet' job='Developer'></Person>
+        
       </header>
     </div>
   );
 }
-function Person() {
+function Person(props
+) {
   const personStyle ={
     border: '2px solid yellow',
     margin: '10px'
   }
   return (
   <div style={personStyle}>
-  <h1>Name : Safaet</h1>
-  <h1>Job: Student</h1>
+  <h1>Name :{props.name}</h1>
+  <h1>Job: {props.job}</h1>
   </div>
   )
 
